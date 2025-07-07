e0a021ad78d324fed0444a9ae7c2faab90b343d5
Poojani-janitha <jayarathnahpj23@gmail.com>
2025-07-07 16:23:34 +0530

    add assest filolder

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
+        <img src={assets.logo}></img>
       
     </div>
   )
