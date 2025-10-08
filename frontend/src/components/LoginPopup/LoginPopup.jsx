cf9c86aaad8c41ebf20028386f3c968b143a33e0
Poojani-janitha <jayarathnahpj23@gmail.com>
2025-10-08 23:11:26 +0530

    add css to the login popup

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -5,17 +5,26 @@ import { assets } from '../../assets/assets'
 const LoginPopup = ({setShowLogin}) => {
     const[currentState,setCurrState] = useState("Sign Up")
   return (
-    <div classname = 'login-popup'>
+    <div className = 'login-popup'>
       <form action="" className="login-popup-container">
         <div className="login-popup-title">
                 <h2>{currentState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className="login-popup-inputs">
-            <input type="text" placeholder='Your name' required />
+            {currentState === "Login"?<></>: <input type="text" placeholder='Your name' required />}
             <input type="text" placeholder='Your email' required />
             <input type="text" placeholder='Password' required />
         </div>
+        <button>{currentState === "Sign Up"?"Create account":"Login"}</button>
+        <div className="login-popup-condition">
+            <input type="checkbox" required />
+            <p>By continuing , i agree to the terms of use & privacy policy.</p>
+        </div>
+        {currentState === "Login"?<p>Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>:
+         <p>Already have an account?<span onClick={()=>setCurrState("Login")}>Logon here</span></p>}
+        
+       
       </form>
     </div>
   )
