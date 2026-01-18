1a0ceac15e7ea6127e849cfcb72099eb10b6d5b7
Poojani-janitha <jayarathnahpj23@gmail.com>
2026-01-18 23:08:21 +0530

    develop admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Sidebar from './components/Sidebar/Sidebar.jsx'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add/Add.jsx'
+import List from './pages/List/List.jsx'
+import Orders from './pages/Orders/Orders.jsx'
+
 
 
 const App = () => {
@@ -10,6 +15,11 @@ const App = () => {
         <hr />
         <div className="app-content">
             <Sidebar />
+            <Routes>
+                <Route path='/add' element={<Add />} />
+                <Route path='/list' element={<List />} />
+                <Route path='/orders' element={<Orders />} />
+            </Routes>
         </div>
       
     </div>
---
+++ admin/src/pages/List/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './List.css'
+
+const list = () => {
+  return (
+    <div>
+      list
+    </div>
+  )
+}
+
+export default list
